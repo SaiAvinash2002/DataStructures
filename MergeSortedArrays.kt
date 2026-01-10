a313153767b3898fb1847551687af7b34256bd38
Sai Avinash Vagicherla <76832424+SaiAvinash2002@users.noreply.github.com>
2026-01-10 12:05:46 -0600

    Add comments for mergeSortedArrays function
    Added comments to explain the mergeSortedArrays function.

--- MergeSortedArrays.kt
+++ MergeSortedArrays.kt
@@ -1,3 +1,9 @@
+//    Merge two sorted array to form third sorted array: 
+//    e.g. 
+//    First Array: 10, 28, 50, 75, 80, 95, 100 
+//    Second array: 35, 60, 70, 78 
+//    Expected Merged sorted array: 10, 28, 35, 50, 60, 70, 75, 78, 80, 95, 100
+
 fun mergeSortedArrays(array1: List<Int>, array2: List<Int>): MutableList<Int> {
         val array3 = mutableListOf<Int>()
         var i = 0
